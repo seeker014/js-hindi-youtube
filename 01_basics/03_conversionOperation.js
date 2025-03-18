ea35abd67cf48b231f7eb80f71de81c25bc70d6b
seeker014 <abhinav.jee22@gmail.com>
2025-03-18 15:24:05 +0000

    conversion of data types and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-// let score=33
+let score=33
 
-// console.log(typeof score); // number is printed
-// console.log(typeof(score)); // number is printed
+console.log(typeof score); // number is printed
+console.log(typeof(score)); // number is printed
 
-// let score="33"
-// let valueInNumber = Number(score) //converting string to number
-// console.log(typeof valueInNumber);
+let score="33"
+let valueInNumber = Number(score) //converting string to number
+console.log(typeof valueInNumber);
 
 let score="33abc"
 let valueInNumber = Number(score) //converting string to number
@@ -24,5 +24,38 @@ let stringNo=String(no) // converted to string
 console.log(stringNo); 
 console.log(typeof stringNo); //gives string as output
 
+Operations :
+
+let value=3
+let negvalue=-value;
+console.log(negvalue); //-3 gets printed
+
+/* 
+console.log(2+2); 4
+console.log(2-2); 0
+console.log(2*2); 4
+console.log(2**3); 2^3=8
+console.log(2/3); 0.6666
+console.log(2%3); 2
+*/
+
+let str1="hello"
+let str2=" abhi"
+
+let str3=str1+str2 //hello abhi is in str3
+console.log("1"+2+2); //122 is printed
+console.log(1+2+"2"); //32 is output , so don't rely on such operations
+console.log("2"+3+2+"1"); //2321
+console.log(2+3+"4"+2+1); //5421 whenever string starts then just concatenation
+
+console.log(+true); // gives 1
+//true+ gives error
+console.log(+"") // gives 0
+
+let num1,num2,num3
+num1=num2=num3=2+2 // all have 4 but this approach is not preferred
+console.table([num1,num2,num3]);
 
 
+let gamecounter=100
+++gamecounter; //++a and a++ increment before use and use before increment 
